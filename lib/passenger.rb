40e35ef3c27ef7583b042319743f76fae4add2ab
Sebastian McKee <sebmcke@gmail.com>
2024-12-18 10:23:03 -0600

    Add passenger initialize method

---
+++ lib/passenger.rb
@@ -0,0 +1,8 @@
+class Passenger
+    attr_reader :name, :age
+
+    def initialize(passenger_info)
+        @name = passenger_info["name"]
+        @age = passenger_info["age"]
+    end
+end
--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -1,5 +1,13 @@
-require 'lib/park.rb'
+require './lib/passenger.rb'
 
-RSpec.describe Park do
-    
+RSpec.describe Passenger do
+    describe "initialization" do
+        it "can create a passenger" do
+            charlie = Passenger.new({"name"=>"Charlie","age"=>18})
+
+            expect(charlie).to be_a(Passenger)
+            expect(charlie.name).to eq("Charlie")
+            expect(charlie.age).to eq(18)
+        end
+    end
 end
