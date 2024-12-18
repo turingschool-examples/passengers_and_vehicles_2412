87128075eb052ba42004324bad8740e10ffdaac4
Donte Handy <donte.handy@gmail.com>
2024-12-18 14:10:51 -0400

    notes for final passenger class

--- lib/passenger.rb
+++ lib/passenger.rb
@@ -1,4 +1,3 @@
-require './lib/passenger'
 # Use TDD to create a Passenger class that responds to the following interaction pattern.
 # For the adult? method, a Passenger is considered an adult if they are age 18 or older.
 
@@ -7,23 +6,27 @@ require 'pry'
 
 
 class Passenger
-  attr_reader :name, :age, :driver
+  attr_reader :name, :age, :driver #symbols used to create instance variables and methods
 
-  def initialize(details)
+  def initialize(details) #initialize method takes in a hash argument
     @name = details["name"]
+    #@name is 
+    #= is the assignment operator
+    #[] is used to access the value of a key in a hash
+    #["name"] is the key we are accessing from the hash - ["parameter/argument"]
     @age = details["age"]
-    @driver = false
+    @driver = false #default value for driver is false
   end
 
   def adult?
-    @age >= 18
+    @age >= 18 #if age is greater than or equal to 18, return true
   end
 
-  def driver?
+  def driver? #method to check if passenger is a driver
     @driver
   end
 
-  def drive
+  def drive #method to change driver status to true
     @driver = true
   end
 end
