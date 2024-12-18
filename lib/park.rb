cce8879ace7a53feba3fd93ab27ee6981f6cdbd0
Sebastian McKee <sebmcke@gmail.com>
2024-12-18 10:55:52 -0600

    Add initialize method

---
+++ lib/park.rb
@@ -0,0 +1,8 @@
+class Park
+    attr_reader :name, :price
+
+    def initialize(name, price)
+        @name = name
+        @price = price
+    end
+end
--- spec/park_spec.rb
+++ spec/park_spec.rb
@@ -3,5 +3,14 @@ require './lib/passenger'
 require './lib/vehicle'
 
 RSpec.describe Park do
-
+    before(:each) do
+        @park = Park.new("Park1", 15)
+    end
+    describe "Initialize" do
+        it "can create a park" do
+            expect(@park).to be_a(Park)
+            expect(@park.name).to eq("Park1")
+            expect(@park.price).to eq(15)
+        end
+    end
 end
