4bcd1c6f99a7f381292a3050f7979a44cb021203
Patrick Little <little.patrick@outlook.com>
2024-12-18 09:49:04 -0700

    class passes tests for speed and adding passengers

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -4,13 +4,15 @@ class Vehicle
   attr_reader :year, 
               :make,
               :model,
-              :speeding
+              :speeding,
+              :passengers
 
   def initialize(year, make, model)
     @year = year
     @make = make
     @model = model
     @speeding = false
+    @passengers = []
   end
 
   def speeding?
@@ -20,4 +22,10 @@ class Vehicle
   def speed
     @speeding = true
   end
+
+  def add_passenger(passenger)
+    @passengers << passenger
+  end
+
+  
 end
