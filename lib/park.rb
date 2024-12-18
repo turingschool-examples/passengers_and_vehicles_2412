96ea749844e81fa2a616cfbe1b63186ba2d7f33f
Jaren Garman <jarengarman@protonmail.com>
2024-12-18 11:47:21 -0500

    feature: Add initialize and add_vehicle methods to Park

---
+++ lib/park.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# Define a park with a name and admission price. Can hold vehicles.
+class Park
+  attr_reader :name, :admission_price, :vehicles
+
+  def initialize(name, admission_price)
+    @name = name
+    @admission_price = admission_price
+    @vehicles = []
+  end
+
+  def add_vehicle(vehicle)
+    @vehicles << vehicle
+  end
+end
