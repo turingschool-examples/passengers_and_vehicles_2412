61d8b1f2156cbf8df35cc0a400c0c7fa3002af93
Jacob Inman <jrinman6295@gmail.com>
2024-12-18 08:56:24 -0800

    passengers added to vehicle spec test in before(:each) step

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -5,6 +5,9 @@ require './lib/passenger'
 RSpec.describe Vehicle do
     before(:each) do
         @vehicle = Vehicle.new("2001", "Honda", "Civic")
+        @charlie = Passenger.new({name: "Charlie", age: 18})
+        @jude = Passenger.new({name: "Jude", age: 20})
+        @taylor = Passenger.new({name: "Taylor", age: 12})
     end
 
     describe '#initialize' do
