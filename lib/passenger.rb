38f8f1d449b13d335014242f8bfba3fcf8ac9126
Jacob Inman <jrinman6295@gmail.com>
2024-12-18 08:29:21 -0800

    passenger class initialized

--- lib/passenger.rb
+++ lib/passenger.rb
@@ -1,10 +1,8 @@
 class Passenger
-    attr_reader
+    attr_reader :name, :age
 
     def initialize(passenger_details)
         @name = passenger_details[:name]
         @age = passenger_details[:age]
     end
-
-    def
 end
--- spec/passener_spec.rb
+++ spec/passener_spec.rb
@@ -3,48 +3,48 @@ require './lib/passenger'
 
 RSpec.describe Passenger do
     before(:each) do
-        charlie = Passenger.new({"name" => "Charlie", "age" => 18})
-        taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+        @charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+        @taylor = Passenger.new({"name" => "Taylor", "age" => 12})
     end
 
     describe '#initialize' do
-        xit 'is an instance of a Passenger' do
-            expect(charlie).to be_an_instance of(Passenger)
+        it 'is an instance of a Passenger' do
+            expect(@charlie).to be_an_instance_of(Passenger)
         end
     end
 
     describe '#name' do
         xit 'can tell its name' do
-            expect(charlie.name).to eq("Charlie")
+            expect(@charlie.name).to eq("Charlie")
         end
     end
 
     describe '#age' do
         xit 'can tell its age' do
-            expect(charlie.age).to eq(18)
+            expect(@charlie.age).to eq(18)
         end
     end
 
     describe '#adult?' do
         xit 'can assess if it is an adult or not' do
-            expect(charlie.adult?).to eq(true)
-            expect(taylor.adult?).to eq(false)
+            expect(@charlie.adult?).to eq(true)
+            expect(@taylor.adult?).to eq(false)
         end
     end
 
     describe '#driver?' do
         xit 'can assess if the passenger is the driver' do
-            expect(charlie.driver?).to eq(false)
+            expect(@charlie.driver?).to eq(false)
         end
     end
 
     describe '#drive' do
         xit 'can reassign who the driver is' do
-            expect(charlie.driver?).to eq(false)
+            expect(@charlie.driver?).to eq(false)
 
             charlie.drive
 
-            expect(charlie.driver?).to eq(true)
+            expect(@charlie.driver?).to eq(true)
         end
     end
 end
