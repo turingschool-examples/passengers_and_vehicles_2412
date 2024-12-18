b1048b9d84e9539ec4c4e96442d7263849e7064b
Patrick Little <little.patrick@outlook.com>
2024-12-18 10:13:09 -0700

    started park_spec tests. The before command methods are laid out

--- spec/park_spec.rb
+++ spec/park_spec.rb
@@ -1,2 +1,27 @@
 require './spec/spec_helper.rb'
 
+# attr_reader and initialize park name and ticket price
+# 
+
+
+RSpec.describe Park do
+  before(:each) do
+    @park_1 = Park.new('Jackson Hole', 10)
+
+
+    @vehicle_1 = Vehicle.new("2001", "Honda", "Civic")
+    @charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+    @jude = Passenger.new({"name" => "Jude", "age" => 20})
+    @taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+
+    @vehicle_2 = Vehicle.new("1955", "VW", 'Beetle')
+    @patrick = Passenger.new({'name' => 'Patrick', 'age' => 24})
+
+    @vehicle_1.add_passenger(@charlie)
+    @vehicle_1.add_passenger(@jude)
+    @vehicle_1.add_passenger(@taylor)
+    
+    @vehicle_2.add_passenger(@patrick)
+  end
+  
+end
